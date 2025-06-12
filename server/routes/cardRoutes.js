77a486ece76713814324c70f3e5545c05f86dabe
Pranav <pranavsg2002@gmail.com>
2025-06-12 19:33:47 +0530

    Fix: Removed getAllCards import that doesn't exist

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllCards, handleQuery } from "../controllers/cardController.js";
+import { handleQuery } from "../controllers/cardController.js";
 
 const router = express.Router();
 
-router.get("/", getAllCards);
+// router.get("/", getAllCards);
 router.post("/query", handleQuery);
 
 export default router;
